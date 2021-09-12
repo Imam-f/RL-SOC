03c6eb7c032cb57f6a51a9efd21c1b1a7dcecc19
Imam-f <mohamadimamfirdaus17@gmail.com>
2021-09-12 20:29:51 +0700

    first commit

---
+++ InterfaceModule.cpp
@@ -0,0 +1,11 @@
+#include "InterfaceModule.h"
+
+void VecReader(Vec128_t* ddr, hlslib::Stream<Vec128_t> &fifo, int readRep) {
+	for (int i=0; i<readRep; i++) {
+		fifo.write(ddr[i]);
+	}
+}
+
+void ScaWriter(hlslib::Stream<Sca32_t> &fifo, Sca32_t* ddr) {
+	ddr[0] = fifo.read();
+}
